25f80a1be0705eaba4c15f703f0c0ccce07b9409
Aurelien LDP <aurelien.ldp@gmail.com>
2019-08-26 10:18:55 +0200

    Updates
    * Add some git aliases
    
    * Set Nerd Tree to Tab key
    
    * Change clear binding

--- packages/zsh/zsh/alias.zsh
+++ packages/zsh/zsh/alias.zsh
@@ -8,21 +8,29 @@ fi
 alias ls="$ls_default -Fh"
 alias l="$ls_default -laFh"
 alias ll="$ls_default -lFh"
+alias la="$ls_default -lAh"
 
 unset ls_default
 
 # Git
 alias g="git"
-alias gs="git status"
+alias gaa="git add -A"
+alias gst="git status"
 alias gc="git commit"
 alias gcm="git commit -m"
 alias gca="git commit --amend"
+alias gd="git diff"
 alias gb="git branch"
-alias gr="git rebase"
-alias gri="git rebase -i"
+alias grb="git rebase"
+alias grbd="git rebase develop"
+alias grb="git rebase"
+alias grbi="git rebase -i"
 alias gco="git checkout"
 alias gcb="git checkout -b"
 alias gff="git merge --ff"
+alias gsta="git stash save"
+alias gstl="git stash list"
+alias gstp="git stash pop"
 
 function gpr {
     local current_branch="$(git rev-parse --abbrev-ref HEAD)"
